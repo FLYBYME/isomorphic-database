8a5a37c44a41443790c16ba621c280af7380d226
FLYBYME <mangoraft@gmail.com>
2026-03-13 19:53:16 -0400

    feat: Configure Jest module name mapping for internal packages and update dependencies, adding `eventemitter3` and `nanoid`.

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,12 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^isomorphic-core$': '<rootDir>/../isomorphic-core/src/index',
+    '^isomorphic-registry$': '<rootDir>/../isomorphic-registry/src/index',
+    '^isomorphic-auth$': '<rootDir>/../isomorphic-auth/src/index',
+    '^isomorphic-mesh$': '<rootDir>/../isomorphic-mesh/src/index',
+    '^raft-consensus$': '<rootDir>/../raft-consensus/src/index'
   }
 };
